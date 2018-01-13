b44bd9953601f626b27f42070add95d8117e260b
Orok Ironbar <rokkside@gmail.com>
2018-01-13 11:18:00 -0800

    Another passed funcFunc test

--- __tests__/func_func_test.js
+++ __tests__/func_func_test.js
@@ -1,14 +1,15 @@
+function returnFive() {return 5}
 it('returns 5', () => {
   expect(returnFive()).toBe(5);
 });
-
+function funcFunc() {return returnFive}
 it('returns a function', () => {
   const result = funcFunc();
 
   expect(result).toBeInstanceOf(Function);
   expect(result()).toBe(5);
 });
-
+function funcArgFunc() {return function(number) {return number*2}}
 it('returns a function that accepts an argument', () => {
   const result = funcArgFunc();
 
