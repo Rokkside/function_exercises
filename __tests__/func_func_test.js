ea0bba59df7f3ec20a44844e6fdead7a5ac7d5e3
Robert Fletcher <lobatifricha@gmail.com>
2018-01-13 10:46:41 -0800

    add func_func_test

---
+++ __tests__/func_func_test.js
@@ -0,0 +1,14 @@
+it('returns a function', () => {
+  const result = funcFunc();
+
+  expect(result).toBeInstanceOf(Function);
+  expect(result()).toBe(5);
+});
+
+it('returns a function that accepts an argument', () => {
+  const result = funcArgFunc();
+
+  expect(result).toBeInstanceOf(Function);
+  expect(result(2)).toBe(4);
+  expect(result(5)).toBe(10);
+});
